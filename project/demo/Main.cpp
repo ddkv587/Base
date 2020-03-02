d9eeba07a7f147b7ae834508e79a0d35a36cfb3e
邓道宽 <532911875@qq.com>
2020-03-02 17:44:11 +0800

    update

--- project/demo/Main.cpp
+++ project/demo/Main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include "CBase.hpp"
 
-static BASE::INT s_mark = 0;
+static Base::INT s_mark = 0;
 
 void task()
 {
@@ -11,11 +11,11 @@ void task()
 
 int main(int argc, char const *argv[])
 {
-    BASE::CThreadPool pool( 10, 100 );
+    Base::CThreadPool pool( 10, 100 );
 
     pool.start();
 
-    for ( BASE::INT i=0; i < 100; ++i ) {
+    for ( Base::INT i=0; i < 100; ++i ) {
         pool.addTask( task );
     }
 
