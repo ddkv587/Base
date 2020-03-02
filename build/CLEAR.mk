1d01a015744e8f6e4ebb8b1d76aa0b353ea78fc3
邓道宽 <532911875@qq.com>
2020-03-02 17:39:03 +0800

    update

--- build/CLEAR.mk
+++ build/CLEAR.mk
@@ -3,7 +3,7 @@ MKDIR              := mkdir -p
 COPY               := cp -f
 STRIP              := strip
 TARGET_ARCH        := 
-CFLAGS             := -Wall -c -fmessage-length=0 -MMD -MP $(PLATFROM_MACRO) -fpermissive -flto -Os
+CFLAGS             := -Wall -c -fmessage-length=0 -MMD -MP $(PLATFROM_MACRO) -fpermissive -Os
 CFLAGS_DEBUG       := -Wall -c -fmessage-length=0 -MMD -MP $(PLATFROM_MACRO) -fpermissive -O0 -g3
 LIBS               := -lm -lstdc++
 LIBS_DIR           := 
