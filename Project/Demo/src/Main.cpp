c4feb2e47e7e982d82a412544f4cac5aae33576e
ddkV587 <ddkV587@gmail.com>
2020-03-06 17:17:49 +0800

    update

--- Project/Demo/src/Main.cpp
+++ Project/Demo/src/Main.cpp
@@ -9,7 +9,7 @@ void task()
     ::std::lock_guard< ::std::mutex > guard( s_mutex );
 
     s_mark++;
-    ::std::cout << "thread: " << s_mark << pthread_self() << "mark: " << s_mark << ::std::endl;
+    ::std::cout << "thread: " << s_mark << pthread_self() << ", mark: " << s_mark << ::std::endl;
 }
 
 int main(int argc, char const *argv[])
