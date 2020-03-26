68a65d5eafc20e2f81baa9fe388dcdc842949b8e
ddkV587 <ddkV587@gmail.com>
2020-03-26 15:59:53 +0800

    update

--- Project/Demo/src/Main.cpp
+++ Project/Demo/src/Main.cpp
@@ -78,7 +78,7 @@ int main(int argc, char const *argv[])
     ::std::cout << mem.dump(4) << ::std::endl;
 #endif
 
-#if 0
+#if 1
     // sample thread pool
     ::Base::CThreadPool pool( 10, 100 );
 
