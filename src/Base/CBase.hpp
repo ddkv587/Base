05e5bd9b3f30a8407e28f77da203e3a76bb9e491
ddkV587 <ddkV587@gmail.com>
2020-03-26 15:38:03 +0800

    update

--- src/Base/CBase.hpp
+++ src/Base/CBase.hpp
@@ -1,8 +1,8 @@
 /*
  * @Author: ddkV587 
  * @Date: 2020-03-18 17:14:38 
- * @Last Modified by:   ddkV587 
- * @Last Modified time: 2020-03-18 17:14:38 
+ * @Last Modified by: ddkV587
+ * @Last Modified time: 2020-03-26 15:37:23
  */
 
 #ifndef __CBASE_HPP__
@@ -13,6 +13,8 @@ namespace Base
     class CMemAllocator;
     class CMemManager;
     class Memory;
+
+    class CLog;
 
     class CThreadPool;
 }
@@ -26,6 +28,8 @@ namespace Base
 #include "Memory/Pool/CMemAllocator.hpp"
 #include "Memory/Pool/CMemManager.hpp"
 #include "Memory/Memory.hpp"
+
+#include "Log/CLog.hpp"
 
 #include "Thread/CSampleThreadPool.hpp"
 
